604623ec708436b5367b6c992e9f606eb6b9161b
bendaryy <mohamedbendary1994@gmail.com>
2024-01-20 12:39:00 +0200

    lkdjf

--- EInvoicing/SubmitInvoices.bat
+++
@@ -1,2 +0,0 @@
-D:\EInvoicing\publish/EInvoicingSigner.exe  D:\EInvoicing 56199100 "Egypt Trust CA G6" "SignatureP11.dll"
-PAUSE
---
+++ EInvoicing/SubmitInvoices2.bat
@@ -0,0 +1,2 @@
+C:\laragon\www\stroker\EInvoicing\publish/EInvoicingSigner.exe  C:\laragon\www\stroker\EInvoicing 89540117 "Egypt Trust CA G6" "SignatureP11.dll"
+PAUSE
